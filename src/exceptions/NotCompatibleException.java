cdf2f43a2369c035068b914cf0dd0d62d322132f
BunsenMcdubbs <theman@andrewdai.co>
2012-03-22 12:33:14 -0400

    added some exceptions

---
+++ src/exceptions/NotCompatibleException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class NotCompatibleException extends Exception {
+	
+	public NotCompatibleException() {}
+
+	public NotCompatibleException(String message) {
+		super(message);
+	}
+}
