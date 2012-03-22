cff3709fa818408a427fc8e515bed1e24d2e5db0
BunsenMcdubbs <theman@andrewdai.co>
2012-03-22 12:04:54 -0400

    epic fail in array numbering fixed

--- src/framework/Puzzle.java
+++ src/framework/Puzzle.java
@@ -7,39 +7,39 @@ public class Puzzle {
 	
 	public Puzzle(){
 		board = new Board();
-		p[1] = new Piece(new Side(Side.outSpade),
+		p[0] = new Piece(new Side(Side.outSpade),
 				new Side(Side.outDiamond),
 				new Side(Side.inSpade),
 				new Side(Side.inHeart));
-		p[2] = new Piece(new Side(Side.outSpade),
+		p[1] = new Piece(new Side(Side.outSpade),
 				new Side(Side.outSpade),
 				new Side(Side.inHeart),
 				new Side(Side.inClub));
-		p[3] = new Piece(new Side(Side.outClub),
+		p[2] = new Piece(new Side(Side.outClub),
 				new Side(Side.outHeart),
 				new Side(Side.inDiamond),
 				new Side(Side.inClub));
-		p[4] = new Piece(new Side(Side.outClub),
+		p[3] = new Piece(new Side(Side.outClub),
 				new Side(Side.outHeart),
 				new Side(Side.inSpade),
 				new Side(Side.inHeart));
-		p[5] = new Piece(new Side(Side.outSpade),
+		p[4] = new Piece(new Side(Side.outSpade),
 				new Side(Side.outDiamond),
 				new Side(Side.inHeart),
 				new Side(Side.inDiamond));
-		p[6] = new Piece(new Side(Side.outHeart),
+		p[5] = new Piece(new Side(Side.outHeart),
 				new Side(Side.outSpade),
 				new Side(Side.inSpade),
 				new Side(Side.inClub));
-		p[7] = new Piece(new Side(Side.outHeart),
+		p[6] = new Piece(new Side(Side.outHeart),
 				new Side(Side.outDiamond),
 				new Side(Side.inClub),
 				new Side(Side.inClub));
-		p[8] = new Piece(new Side(Side.outDiamond),
+		p[7] = new Piece(new Side(Side.outDiamond),
 				new Side(Side.outClub),
 				new Side(Side.inClub),
 				new Side(Side.inDiamond));
-		p[9] = new Piece(new Side(Side.outHeart),
+		p[8] = new Piece(new Side(Side.outHeart),
 				new Side(Side.outDiamond),
 				new Side(Side.inDiamond),
 				new Side(Side.inHeart));
