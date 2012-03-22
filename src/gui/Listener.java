c9a1a71dc135fe610d981511cbcf1cf88a4f3ac0
Andrew Dai <theman@andrewdai.co>
2012-03-22 17:49:03 -0400

    changed comments

--- src/gui/Listener.java
+++ src/gui/Listener.java
@@ -10,7 +10,7 @@ import java.awt.event.MouseMotionListener;
 import org.w3c.dom.events.EventTarget;
 import org.w3c.dom.events.MouseEvent;
 import org.w3c.dom.views.AbstractView;
-//TODO implement interfaces for Listening to events and such
+//TODO implement interfaces for Listening to events and movements
 public class Listener {
 
 	public Listener() {}
