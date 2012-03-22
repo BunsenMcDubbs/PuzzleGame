d7372985da1f55a567516b4038f10af21f9b3d6a
Andrew Dai <theman@andrewdai.co>
2012-03-21 20:44:15 -0400

    added comments in javadoc format for the methods and added a valid location check on the getNeighbors method

--- src/framework/Board.java
+++ src/framework/Board.java
@@ -10,38 +10,84 @@ public class Board {
 				board[i][j] = null;	
 	}
 	
+	/**
+	 * Checks if the provided coordinates define a valid location
+	 * on the board.
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean isValidLocation(int x, int y){
 		if(x > -1 && y > -1 && x < 3 && y < 3)
 			return true;
 		return false;
 	}
 	
+	/**
+	 * Tells whether or not the specified location is empty or
+	 * filled.
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean isEmpty(int x, int y){
 		if(board[x][y] == null)
 			return true;
 		return false;
 	}
 	
+	/**
+	 * Returns the Piece at the specified location on the board.
+	 * If either the location is empty or the coordinates are
+	 * off the board, the method will return null.
+	 * @param x coordinate of the piece
+	 * @param y coordinate of the piece
+	 * @return the piece at the location
+	 */
 	public Piece getLocation(int x, int y){
 		if (isValidLocation(x,y))
 			return board[x][y];
 		return null;
 	}
 	
-	public void setLocation(int x, int y, Piece piece){
-		if(isValidLocation(x,y))
+	/**
+	 * Puts a piece at a specified location on the board. Its
+	 * return depends on whether or not the setting was successful
+	 * (aka if the location was on the board).
+	 * @param x
+	 * @param y
+	 * @param piece
+	 */
+	public boolean setLocation(int x, int y, Piece piece){
+		if(isValidLocation(x,y)){
 			board[x][y] = piece;
+			return true;
+		}
+		return false;
 	}
 	
+	/**
+	 * Returns the neighbors of a specified cell in an array.
+	 * The Pieces are in North, East, South, West order and
+	 * any spots that are empty are null in the array. If the 
+	 * specified location is invalid (not on the Board) the
+	 * method will return null.
+	 * @param x coordinate of the cell
+	 * @param y coordinate of the cell
+	 * @return array with all the neighboring Pieces
+	 */
 	public Piece[] getNeighbors(int x, int y){
-		Piece[] array = new Piece[4];
 		if(isValidLocation(x,y)){
-			array[0] = getLocation(x,y-1);
-			array[1] = getLocation(x+1,y);
-			array[2] = getLocation(x,y+1);
-			array[3] = getLocation(x-1,y);
+			Piece[] array = new Piece[4];
+			if(isValidLocation(x,y)){
+				array[0] = getLocation(x,y-1);
+				array[1] = getLocation(x+1,y);
+				array[2] = getLocation(x,y+1);
+				array[3] = getLocation(x-1,y);
+			}
+			return array;
 		}
-		return array;
+		return null;
 	}
 	
 	/**
