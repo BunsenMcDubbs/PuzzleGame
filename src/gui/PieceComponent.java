c9fcb4b647e8b67cea8c475af048b296af998065
Andrew Dai <theman@andrewdai.co>
2012-03-21 20:03:48 -0400

    added boolean flag inBoard to determine whether or not the piece has been placed in a board.

--- src/gui/PieceComponent.java
+++ src/gui/PieceComponent.java
@@ -3,12 +3,27 @@ package gui;
 import framework.Piece;
 import javax.swing.JComponent;
 
-public class PieceComponent extends JComponent {
+public class PieceComponent extends JComponent implements Placeable{
 	
 	private Piece piece;
+	private boolean inBoard;
 	
 	public PieceComponent(Piece p) {
 		piece = p;
+		inBoard = false;
+	}
+	
+	public boolean isInBoard(){
+		return inBoard;
+	}
+	
+	public void setInBoard(boolean tf){
+		inBoard = tf;
+	}
+	
+	public boolean toogleInBoard(){
+		inBoard = !inBoard;
+		return inBoard;
 	}
 	
 }
