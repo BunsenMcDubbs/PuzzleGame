3242112687c9624113e4bea5fab4fd7bb14edcb3
BunsenMcdubbs <theman@andrewdai.co>
2012-03-29 11:06:55 -0400

    changed to abstract

--- src/gui/PieceShape.java
+++ src/gui/PieceShape.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
@@ -7,52 +8,64 @@ import java.awt.geom.Path2D;
 
 import javax.swing.JFrame;
 
-public class PieceShape {
+public abstract class PieceShape {
 	
-	public static Shape getHeart(){
-		
-		Ellipse2D.Double s = new Ellipse2D.Double(10,0,30,50);
-		
-		return s;
+	private int side;
+	
+	public PieceShape(int side){
+		this.side = side;
 	}
 	
-	public static Shape getClub(){
-		
-		return new Rectangle(0,0,50,50);
-
+	public final int getSide(){
+		return side;
 	}
 	
-	public static Shape getDiamond(){
-		
-		int [] x = {25,40,25,10};
-		int [] y = {0,25,50,25};
-		
-		Path2D.Double s = new Path2D.Double(Path2D.WIND_EVEN_ODD);
-		s.moveTo(x[0], y[0]);
-		for (int i = 1; i < x.length && i < y.length; i++){
-			s.lineTo(x[i], y[i]);
-		}
-		s.closePath();
-		
-		return s;
-
-	}
+	public abstract void paint(Graphics2D g);
 	
-	public static Shape getSpade(){
-		
-		int [] x = {25,50,0};
-		int [] y = {0,50,50};
-		
-		Path2D.Double s = new Path2D.Double(Path2D.WIND_EVEN_ODD);
-		s.moveTo(x[0], y[0]);
-		for (int i = 1; i < x.length && i < y.length; i++){
-			s.lineTo(x[i], y[i]);
-		}
-		s.closePath();
-		
-		return s;
-
-	}
+//	public static Shape getHeart(){
+//		
+//		Ellipse2D.Double s = new Ellipse2D.Double(10,0,30,50);
+//		
+//		return s;
+//	}
+//	
+//	public static Shape getClub(){
+//		
+//		return new Rectangle(0,0,50,50);
+//
+//	}
+//	
+//	public static Shape getDiamond(){
+//		
+//		int [] x = {25,40,25,10};
+//		int [] y = {0,25,50,25};
+//		
+//		Path2D.Double s = new Path2D.Double(Path2D.WIND_EVEN_ODD);
+//		s.moveTo(x[0], y[0]);
+//		for (int i = 1; i < x.length && i < y.length; i++){
+//			s.lineTo(x[i], y[i]);
+//		}
+//		s.closePath();
+//		
+//		return s;
+//
+//	}
+//	
+//	public static Shape getSpade(){
+//		
+//		int [] x = {25,50,0};
+//		int [] y = {0,50,50};
+//		
+//		Path2D.Double s = new Path2D.Double(Path2D.WIND_EVEN_ODD);
+//		s.moveTo(x[0], y[0]);
+//		for (int i = 1; i < x.length && i < y.length; i++){
+//			s.lineTo(x[i], y[i]);
+//		}
+//		s.closePath();
+//		
+//		return s;
+//
+//	}
 	
 	public static void main(String [] args){
 		PuzzleCanvas canvas = new PuzzleCanvas();
