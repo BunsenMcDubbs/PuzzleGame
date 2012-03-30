2d0956c2e8d36419ee17601737189b55081c6e87
BunsenMcdubbs <theman@andrewdai.co>
2012-03-30 11:44:19 -0400

    implementing DnD

--- src/gui/PieceComponent.java
+++ src/gui/PieceComponent.java
@@ -5,6 +5,7 @@ import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.dnd.DragGestureEvent;
 import java.awt.dnd.DragGestureListener;
+import java.awt.dnd.DragSource;
 import java.awt.dnd.DragSourceDragEvent;
 import java.awt.dnd.DragSourceDropEvent;
 import java.awt.dnd.DragSourceEvent;
@@ -26,9 +27,12 @@ import framework.Side;
 public class PieceComponent extends JComponent implements Transferable, DragSourceListener, DragGestureListener{
 	
 	private Piece piece;
-	Rectangle body;
+	private Rectangle body;
 	private Shape [] sides;
 	private boolean inBoard;
+	
+	private TransferHandler t;
+	private DragSource source;
 	
 	public PieceComponent(Piece p) {
 		piece = p;
@@ -39,10 +43,21 @@ public class PieceComponent extends JComponent implements Transferable, DragSour
 	}
 	
 	private void dnd() {
-		TransferHandler transfer = new TransferHandler("getPiece");
-		setTransferHandler(transfer);
-	}
+		t = new TransferHandler(){
 
+            public Transferable createTransferable(JComponent c){
+                  return exportPiece();
+            }
+      };
+		TransferHandler t = new TransferHandler("getPiece");
+		setTransferHandler(t);
+	}
+	
+	private Transferable exportPiece() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	private void pieceMaker() {
 		
 		body = new Rectangle(50,50,100,100);
@@ -98,45 +113,58 @@ public class PieceComponent extends JComponent implements Transferable, DragSour
 	public Piece getPiece(){
 		return piece;
 	}
-	
-	private class MouseAdapter{
-        public void mousePressed(MouseEvent e){
-        	PieceComponent p = (PieceComponent)e.getSource();
-            TransferHandler handle = p.getTransferHandler();
-            handle.exportAsDrag(p, e, TransferHandler.COPY);
-        }
-	}
 
 	@Override
-	public void dragGestureRecognized(DragGestureEvent arg0) {
-		// TODO Auto-generated method stub
+	public void dragGestureRecognized(DragGestureEvent dge) {
+		source.startDrag(dge, DragSource.DefaultCopyDrop, exportPiece(), this);
 		
 	}
 
+	/**
+	 * Successful DnD operation, copies the framework <code>Piece</code> in the
+	 * PieceComponent into framework <code>Board</code> in the PuzzleGameFrame
+	 * Snaps the piece into place to the location's center
+	 * and TODO highlight the available sides of the piece to snap into
+	 */
 	@Override
 	public void dragDropEnd(DragSourceDropEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		repaint();
 	}
-
+	
+	/**
+	 * Highlights the piece when it is hovering over a droppable location
+	 * outlines/highlights the piece and the location on the board
+	 */
 	@Override
 	public void dragEnter(DragSourceDragEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	/**
+	 * Un-highlights the piece and snaps it back to the home location outside
+	 * of the board
+	 */
 	@Override
 	public void dragExit(DragSourceEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	/**
+	 * Highlights the piece when it is hovering over a droppable location
+	 * outlines/highlights the piece and the location on the board that's center
+	 * is closest to the location this PieceComponent
+	 */
 	@Override
 	public void dragOver(DragSourceDragEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	/**
+	 * Listens for mouse wheel scrolls to rotate the piece
+	 */
 	@Override
 	public void dropActionChanged(DragSourceDragEvent arg0) {
 		// TODO Auto-generated method stub
@@ -146,19 +174,17 @@ public class PieceComponent extends JComponent implements Transferable, DragSour
 	@Override
 	public Object getTransferData(DataFlavor flavor)
 			throws UnsupportedFlavorException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		return piece;
 	}
 
 	@Override
 	public DataFlavor[] getTransferDataFlavors() {
-		// TODO Auto-generated method stub
-		return null;
+		return new DataFlavor[]{new DataFlavor(Piece.class, "Piece")};
 	}
 
 	@Override
 	public boolean isDataFlavorSupported(DataFlavor flavor) {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 }
