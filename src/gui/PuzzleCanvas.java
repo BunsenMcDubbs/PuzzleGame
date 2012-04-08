bb3df36d4d98295e79f9757ef6f641362fd5cce4
Andrew Dai <theman@andrewdai.co>
2012-04-08 15:31:50 -0400

    working click and drop stuff still doesn't check properly

--- src/gui/PuzzleCanvas.java
+++ src/gui/PuzzleCanvas.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -14,21 +15,27 @@ import java.awt.event.MouseWheelListener;
 
 import javax.swing.JComponent;
 
+import framework.Board;
+import framework.Puzzle;
+
 public class PuzzleCanvas extends JComponent implements MouseListener, MouseWheelListener, MouseMotionListener{
-	
+
 	private PieceShape[] p;
-	private Point[] boardLoc;
+	private Point[] boardPoints;
 	private Point[] homeLoc;
-	private Board board;
-	
+	private Rectangle[] boardLocs;
+	private Puzzle puzzle;
+
 	private Point dragOrigin;
-	
-	public PuzzleCanvas(PieceShape[] pieces, Board board){
+
+	public PuzzleCanvas(PieceShape[] pieces, Puzzle puzzle){
 		this.p = pieces;
-		this.board = board;
+		this.puzzle = puzzle;
 		trayMaker();
 		boardMaker();
-		
+
+		addMouseListener(this);
+
 		setSize(1200,750);
 	}
 
@@ -47,34 +54,34 @@ public class PuzzleCanvas extends JComponent implements MouseListener, MouseWhee
 			p[i].setHome(homeLoc[i]);
 		}
 	}
-	
+
 	private void boardMaker(){
-		boardLoc = new Point[9];
-		boardLoc[0] = new Point(50, 200);
-		boardLoc[1] = new Point(200, 200);
-		boardLoc[2] = new Point(350, 200);
-		boardLoc[3] = new Point(50, 350);
-		boardLoc[4] = new Point(200, 350);
-		boardLoc[5] = new Point(350, 350);
-		boardLoc[6] = new Point(50, 500);
-		boardLoc[7] = new Point(200, 500);
-		boardLoc[8] = new Point(350, 500);
+		boardPoints = new Point[9];
+		boardPoints[0] = new Point(50, 200);
+		boardPoints[1] = new Point(200, 200);
+		boardPoints[2] = new Point(350, 200);
+		boardPoints[3] = new Point(50, 350);
+		boardPoints[4] = new Point(200, 350);
+		boardPoints[5] = new Point(350, 350);
+		boardPoints[6] = new Point(50, 500);
+		boardPoints[7] = new Point(200, 500);
+		boardPoints[8] = new Point(350, 500);
 	}
 
 	public void paint(Graphics g){
-		
+
 		Graphics2D g2 = (Graphics2D)g;
 		paintBoard(g2);
 		test(g2);
-		
+
 		for(PieceShape e : p){
 			e.paint(g2);
 		}
 	}
-	
+
 	private void test(Graphics2D g2) {
 		g2.setColor(Color.red);
-		for(Point p : boardLoc){
+		for(Point p : boardPoints){
 			g2.drawRect(p.x, p.y, 10, 10);
 		}
 		g2.setColor(Color.blue);
@@ -84,38 +91,66 @@ public class PuzzleCanvas extends JComponent implements MouseListener, MouseWhee
 	}
 
 	private void paintBoard(Graphics2D g2){
-		g2.drawLine(50, 200, 50, 650);
-		g2.drawLine(200, 200, 200, 650);
-		g2.drawLine(350, 200, 350, 650);
-		g2.drawLine(500, 200, 500, 650);
-		
-		g2.drawLine(50, 200, 500, 200);
-		g2.drawLine(50, 350, 500, 350);
-		g2.drawLine(50, 500, 500, 500);
-		g2.drawLine(50, 650, 500, 650);
+		boardLocs = new Rectangle[9];
+
+		for(int i = 0; i < boardPoints.length; i++){
+			boardLocs[i] = new Rectangle(boardPoints[i].x,boardPoints[i].y, 150, 150);
+			g2.draw(boardLocs[i]);
+		}
 	}
-	
+
 	public PieceShape getClickedPiece(Point point){
 		for(PieceShape e : p){
 			if(e.contains(point)){
+				System.out.println(point);
+				System.out.println(e);
 				return e;
 			}
 		}
 		return null;
 	}
-	
-	public boolean put(PieceShape p){
-		//TODO adsfadsfasdf
-		return false;
-	}
 
+	private PieceShape selected;
 	@Override
 	public void mouseClicked(MouseEvent m) {
-		Point p = m.getPoint();
-		PieceShape piece = getClickedPiece(p);
-		if(piece == null)
-			return;
-		//TODO rotate?
+		if(selected == null){
+			Point p = m.getPoint();
+			PieceShape piece = getClickedPiece(p);
+			if(piece == null)
+				return;
+			selected = piece;
+		}
+		else{
+			Point p = m.getPoint();
+			int spot = getClickedSpot(p);
+			if(spot == -1)
+				return;
+			put(spot);
+			selected = null;
+		}
+	}
+
+	private boolean put(int place){
+		
+		int y = place/3;
+		int x = place%3;
+		
+//		System.out.println(place);
+//		System.out.println("" + x + " , " + y);
+		if(puzzle.canFit(x, y, selected.getPiece())){// TODO check with board
+			selected.setLoc(new Point(boardLocs[place].x, boardLocs[place].y - 50));
+		}
+		repaint();
+		return true;
+	}
+
+	private int getClickedSpot(Point p) {
+		for(int i = 0; i < boardLocs.length; i++){
+			Rectangle r = boardLocs[i];
+			if (r.contains(p))
+				return i;
+		}
+		return -1;
 	}
 
 	@Override
@@ -157,5 +192,5 @@ public class PuzzleCanvas extends JComponent implements MouseListener, MouseWhee
 		}
 		repaint();
 	}
-	
+
 }
