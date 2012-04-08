29be0a0230db7cc6d6837c11aafa7d1104658a74
Andrew Dai <theman@andrewdai.co>
2012-04-07 21:20:22 -0400

    reworked the heart class --> model for all the other pieces

--- src/gui/shapes/Heart.java
+++ src/gui/shapes/Heart.java
@@ -3,7 +3,11 @@ package gui.shapes;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
@@ -14,55 +18,79 @@ import gui.*;
 
 
 public class Heart extends PegShape{
-
-	public Heart(int side, PieceShape p) {
-		super(side, p);
+	
+	private int[] x;
+	private int[] y;
+	
+	public Heart(int side) {
+		super(side);
+		resetPoints();
 	}
-
 	
 	public void paint(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
+		
+		updateLoc();
+		
+		if(getSide() == 0 || getSide() == 1){
+			g2.setColor(Color.RED);
+		}
+		else{
+			g2.setColor(gui.PuzzleGameFrame.backColor);
+		}
+		g2.fill(shapes[0]);
+		g2.fill(shapes[1]);
+		g2.fillPolygon(x, y, 3);
+	}
+	
+	private void updateLoc(){
+		resetPoints();
+		Point loc = getLoc();
+		
+		Shape[]	sTemp = super.shapes;
+		for(Shape e : sTemp){
+			Ellipse2D.Double f = (Ellipse2D.Double)e;
+			f.x += loc.x;
+			f.y += loc.y;
+		}
+		
+		for(int i = 0; i < 3; i++){
+			x[i] += loc.x;
+			y[i] += loc.y;
+		}
+	}
+	
+	private void resetPoints(){
 		if(getSide() == 0 || getSide() == 2){
-			if(getSide() == 0){
-				g2.setColor(Color.RED);
-			}
-			else{
-				g2.setColor(new Color(238,238,238));
-			}
-			g2.fillOval(0,0,26,25);
-			g2.fillOval(25, 0, 25,25);
-			int[] xPoints = {1,26,49};
+			Shape[] shapes = {new Ellipse2D.Double(0,0,26,25),
+				new Ellipse2D.Double(25, 0, 25,26)};
+			int[] xPoints = {0,25,50};
 			int[] yPoints = {18,50,18};
-			g2.fillPolygon(xPoints, yPoints, 3);
+			super.shapes = shapes;
+			this.x = xPoints;
+			this.y = yPoints;
 		}
 		if(getSide() == 1 || getSide() == 3){
-			if(getSide() == 1){
-				g2.setColor(Color.RED);
-			}
-			else{
-				g2.setColor(new Color(238,238,238));
-			}
-			g2.fillOval(25, 0, 25, 26);
-			g2.fillOval(25, 25, 25, 25);
+			Shape[] shapes ={new Ellipse2D.Double(25, 0, 25, 26),
+					new Ellipse2D.Double(25, 25, 25, 25)};
 			int[] xPoints = {33,0,33};
-			int[] yPoints = {1,26,49};
-			g2.fillPolygon(xPoints, yPoints, 3);
+			int[] yPoints = {0,26,50};
+			super.shapes = shapes;
+			this.x = xPoints;
+			this.y = yPoints;
 		}
 	}
 	
 	public static void main(String[] args){
-//		JFrame frame = new JFrame("asdf");
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.setSize(500,500);
-//		frame.setVisible(true);
-//		Heart heart = new Heart(1, null);
-//		frame.add(heart);
+		test.Testing.main(null);
 	}
 
 
 	@Override
-	public boolean contains(Point2D arg0) {
-		// TODO Auto-generated method stub
+	public boolean contains(Point2D p) {
+		if(super.contains(p))
+			return true;
+		//TODO check with polygon shape
 		return false;
 	}
 
